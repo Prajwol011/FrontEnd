ea64033ee8d426b354ef70ac3a00c84df8356a23
Prajwol Shrestha <prajwolshrestha550@gmail.com>
2026-05-19 16:58:56 +0545

    starting of js

--- JS/first.js
+++ JS/first.js
@@ -3,4 +3,21 @@ age = 19;
 price = 99.99;
 x = null;
 y = undefined;
-console.log(y);
+
+isFollow = true;
+
+console.log(fullname);
+
+let a = 10;
+a = 20; // This is allowed because 'a' is declared with 'let' and can be reassigned
+// let a = 20; // This will cause an error because 'a' is already declared with 'let'
+
+const b = 30;
+// b = 40; // This will cause an error because 'b' is a constant and cannot be reassigned
+
+var c = 50;
+// var c = 60; // This is allowed because 'var' allows redeclaration
+
+console.log(a);
+console.log(b);
+console.log(c);
